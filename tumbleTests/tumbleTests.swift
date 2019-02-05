0231cd761c1abf077b8b2fcf818d5fbe8cf5e763
Alec Tenefrancia <alectenefrancia@sfs-wifi-dhcp-10-143-56-96.wifi.sfsu.edu>
2019-02-04 20:17:41 -0800

    Initial Commit

---
+++ tumbleTests/tumbleTests.swift
@@ -0,0 +1,34 @@
+//
+//  tumbleTests.swift
+//  tumbleTests
+//
+//  Created by Alec Tenefrancia on 2/4/19.
+//  Copyright © 2019 Alec Tenefrancia. All rights reserved.
+//
+
+import XCTest
+@testable import tumble
+
+class tumbleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
